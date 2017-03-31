2b567bcb5e2385f08db443e5c69b16cc12098b48
Jacob <jacoboakley21@gmail.com>
2017-03-31 16:36:37 -0400

    chapter 2 performing operations added

---
+++ inEasySteps/Arithmetic/Arithmetic/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arithmetic
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.Title = "Arithmetic";
+
+      int a = 8;
+      int b = 4;
+
+      Console.WriteLine("Addition:\t" + (a + b) );
+      Console.WriteLine("Subtraction:\t" + (a - b) );
+      Console.WriteLine("Multiplication:\t" + (a * b) );
+      Console.WriteLine("Division:\t" + (a / b) );
+      Console.WriteLine("Modulus:\t" + (a % b) );
+
+      Console.WriteLine("\nPostfix:\t" + (a++));
+      Console.WriteLine("Postfix Result.....:\t" + a);
+
+      Console.WriteLine("\nPrefix: \t" + (++b));
+      Console.WriteLine("Prefix Result.....: \t" + b);
+    }
+  }
+}
